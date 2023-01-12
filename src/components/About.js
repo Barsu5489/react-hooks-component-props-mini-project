f7f99bcb4dce03f6f9f3166b5cc610311fe85007
barsu5489 <ebarsulai@gmail.com>
2023-01-12 10:34:10 +0300

    setting up About component with dummy data

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function About ({image = 'https://via.placeholder.com/215'}){
+    return(
+        <aside>
+            <img src={url} alt="blog logo" />
+            <p>About text goes here</p>
+        </aside>
+    )
+}
